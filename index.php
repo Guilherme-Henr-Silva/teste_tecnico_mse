270a494e76c9769629302871362c9e084b2e78c0
Guilherme Silva <gui.ghs.silva@gmail.com>
2026-03-03 18:01:48 -0300

    Alteração para melhor estruturação de código e adicionando função 'Ver Fornecedores'

--- index.php
+++ index.php
@@ -2,43 +2,29 @@
 <html>
 <head>
     <title>Cadastro de Fornecedores</title>
-
     <link rel="stylesheet" href="css/style.css">
-
-    <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
     <script src="js/validacao.js"></script>
-
 </head>
-
 <body>
 
 <h2>Cadastro de Fornecedor</h2>
 
 <form id="formFornecedor" action="controllers/salvar_fornecedor.php" method="POST">
 
-    <label>Razão Social</label>
-    <input type="text" name="razao_social" required>
-
-    <label>Nome Fantasia</label>
-    <input type="text" name="nome_fantasia">
-
-    <label>CNPJ</label>
-    <input type="text" name="cnpj" id="cnpj" required>
-
-    <label>Email</label>
-    <input type="email" name="email">
-
-    <label>Telefone</label>
-    <input type="text" name="telefone">
-
-    <label>Endereço</label>
-    <input type="text" name="endereco">
+    <input type="text" name="razao_social" placeholder="Razão Social" required>
+    <input type="text" name="nome_fantasia" placeholder="Nome Fantasia">
+    <input type="text" name="cnpj" id="cnpj" placeholder="CNPJ" required>
+    <input type="email" name="email" placeholder="Email">
+    <input type="text" name="telefone" placeholder="Telefone">
+    <input type="text" name="endereco" placeholder="Endereço">
 
     <button type="submit">Cadastrar</button>
 
 </form>
+
+<br>
+<a href="views/listar_fornecedores.php">Ver Fornecedores</a>
 
 </body>
 </html>
