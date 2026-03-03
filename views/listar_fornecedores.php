7db8e977df7709173ae0b2dda95ba2448f378333
Guilherme Silva <gui.ghs.silva@gmail.com>
2026-03-03 18:05:31 -0300

    Implementando listagem de fornecedores cadastrados

--- views/listar_fornecedores.php
+++ views/listar_fornecedores.php
@@ -1,34 +1,35 @@
 <?php
 include("../config/database.php");
 
-$sql = "SELECT * FROM fornecedores";
-$result = $conn->query($sql);
+$result = $conn->query("SELECT * FROM fornecedores");
 ?>
 
 <h2>Lista de Fornecedores</h2>
 
-<table border="1">
+<a href="../index.php">Novo Cadastro</a>
 
+<table>
 <tr>
 <th>ID</th>
 <th>Razão Social</th>
 <th>CNPJ</th>
 <th>Email</th>
+<th>Ações</th>
 </tr>
 
-<?php
+<?php while($row = $result->fetch_assoc()): ?>
 
-while($row = $result->fetch_assoc()){
+<tr>
+<td><?= $row['id'] ?></td>
+<td><?= $row['razao_social'] ?></td>
+<td><?= $row['cnpj'] ?></td>
+<td><?= $row['email'] ?></td>
+<td>
+<a href="editar_fornecedor.php?id=<?= $row['id'] ?>">Editar</a>
+<a href="../controllers/deletar_fornecedor.php?id=<?= $row['id'] ?>">Excluir</a>
+</td>
+</tr>
 
-echo "<tr>
-<td>".$row['id']."</td>
-<td>".$row['razao_social']."</td>
-<td>".$row['cnpj']."</td>
-<td>".$row['email']."</td>
-</tr>";
-
-}
-
-?>
+<?php endwhile; ?>
 
 </table>
