7a15821f87700ec667c41c133d448570c492738e
Guilherme Silva <gui.ghs.silva@gmail.com>
2026-03-03 17:49:50 -0300

    Foi criado a estrutura inicial do banco de dados fornecedores

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$db = "fornecedores_db";
+$user = "root";
+$pass = "";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
+}
+?>
